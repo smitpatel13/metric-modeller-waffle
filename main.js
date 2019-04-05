a6294527dcd1e9f214a87c660e6d7585ec361c04
Josh Piedimonte <joshpied@gmail.com>
2019-04-05 13:26:12 -0400

    cleaned up caluclation, shows months too

--- main.js
+++ main.js
@@ -39,43 +39,11 @@ function doCalculations(formValues) {
     var function_point = this.function_point.value;
     var language = this.language.value;
     var cost_per_hour = this.cost_per_hour.value;
-    var point;
-    if(language === "0"){
-      point = 53;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "1"){
-      point = 54;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "2"){
-      point = 53;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "3"){
-      point = 67;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "4"){
-      point = 46;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "5"){
-      point = 22;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else if(language === "6"){
-      point = 20;
-      document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
-      document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours";
-      document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
-    }else{
-      document.getElementById("locresult").innerHTML = "Something went wrong";
-    }
+    var point = DATA[language].point;
+
+    document.getElementById("locresult").innerHTML = "Lines of Code: " + function_point * point;
+    document.getElementById("timeresult").innerHTML = "\n Time: " + function_point * point / 2 + " Hours or " + ((function_point * point / 2) / 160).toFixed(2) + " months";
+    document.getElementById("costresult").innerHTML = "Cost: $" + function_point * point * cost_per_hour / 2;
+
+
 }
